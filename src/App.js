5f78c5f3eacc9aa898cfc98bfea1cfdaf5edfa23
nguyendev2018 <nguyendev2018@gmail.con>
2022-03-10 21:39:13 +0700

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import ContextProvider from './Hook/Context/ContextProvider';
 import DemoUseContext from './Hook/DemoUseContext';
 
 import DemoUseRef from './Hook/DemoUseRef';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <DemoUseContext />
-      </div>
+      <ContextProvider>
+
+      </ContextProvider>
     )
   }
 }
--- src/Hook/Context/ContextProvider.js
+++ src/Hook/Context/ContextProvider.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 const context = React.createContext()
+let cartReducer = (state, action) => {
+    switch (action.type) {
+        case "addCart": {
+            let index = state.findIndex(item => item.id === action.product.id);
+            if (index !== -1) {
+                state[index].quantity += 1;
+                return { ...state }
+            }
+            return [...state, { ...action.product, quantity: 1 }]
+        };
+        default: return false
+    }
+}
 export default function ContextProvider() {
+    let [cart, dispatch] = useReducer(cartReducer, initialCart);
+    const store = {
+        cartReducer: [cart, dispatch]
+    }
     return (
-        <div>ContextProvider</div>
+        //dùng content bao bọc tất cả component bên trong (cụ thể là App)
+        <context.Provider value={store}>
+            {props.children}
+        </context.Provider>
+
     )
 }
