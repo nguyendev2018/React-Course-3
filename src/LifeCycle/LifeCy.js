18f392a6a33b085c0a58b821afb38a20f735bb9c
nguyendev2018 <nguyendev2018@gmail.con>
2022-08-30 21:43:34 +0700

    --- Auto Git Commit ---

--- src/LifeCycle/LifeCy.js
+++ src/LifeCycle/LifeCy.js
@@ -4,12 +4,27 @@ class LifeCy extends Component {
   state = {
     number: 1,
   };
-  getDerivedStateFromProps(newProps, currentState) {
+  static getDerivedStateFromProps(newProps, currentState) {
     console.log("getDerivedStateFromProps");
     return null;
   }
+  componentDidMount() {
+    console.log("componentDidMount");
+    return null;
+  }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <p>{this.state.number}</p>
+        <button
+          onClick={() => {
+            this.setState({ number: this.state.number + 1 });
+          }}
+        >
+          Click
+        </button>
+      </div>
+    );
   }
 }
 
