2f0177616b9ed9db0f242477575be47ed656a0af
nguyendev2018 <nguyendev2018@gmail.con>
2022-08-31 14:07:08 +0700

    --- Auto Git Commit ---

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import DemoBugger from "./Bugger/DemoBugger";
 import BuggerCyber from "./BuggerCyber/BuggerCyber";
 import DemoHookUseEffect from "./Hook/DemoHookUseEffect";
 import DemoHookUseState from "./Hook/DemoHookUseState";
+import DemoUseEffect from "./Hook/DemoUseEffect";
 import LifeCy from "./LifeCycle/LifeCy";
 export default class App extends Component {
   render() {
-    return <DemoHookUseState></DemoHookUseState>;
+    return <DemoUseEffect></DemoUseEffect>;
   }
 }
---
+++ src/Hook/DemoUseEffect.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DemoUseEffect(props) {
+  return <div>DemoUseEffect</div>;
+}
