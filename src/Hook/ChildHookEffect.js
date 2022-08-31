b1a4307d631f0e45eb1be195fa9dab6f482874fe
nguyendev2018 <nguyendev2018@gmail.con>
2022-08-31 16:00:25 +0700

    --- Auto Git Commit ---

--- src/Hook/ChildHookEffect.js
+++ src/Hook/ChildHookEffect.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function ChildHookEffect() {
-    useEffect(() => {
-        // Viết cho didUpdate
-        console.log('didUpdate');
+  useEffect(() => {
+    // Viết cho didUpdate
+    console.log("didUpdate");
+    return () => {
+      console.log("will unmount");
+    };
+  });
 
-    });
-
-    return (
-        <div>
-            <h1>Update</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Update</h1>
+    </div>
+  );
 }
--- src/Hook/DemoUseEffect.js
+++ src/Hook/DemoUseEffect.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ChildHookEffect from "./ChildHookEffect";
 
 export default function DemoUseEffect(props) {
-  return <div>DemoUseEffect</div>;
+  let [count, setCount] = useState(1);
+  let [number, setNumber] = useState(1);
+  useEffect(() => {
+    console.log("component DidMount");
+  }, []);
+  console.log("render");
+  useEffect(() => {
+    console.log("component DidUpdate");
+  }, [number]);
+  const handleIncreaser = () => {
+    let newNumber = number + 1;
+    setNumber(newNumber);
+  };
+  return (
+    <div>
+      <h4 className="title">{number}</h4>
+      <button onClick={handleIncreaser}>click</button>
+      <button
+        onClick={() => {
+          let newCount = count + 1;
+          setCount(newCount);
+        }}
+      >
+        Change Count
+      </button>
+      {count === 1 ? <ChildHookEffect></ChildHookEffect> : ""}
+    </div>
+  );
 }
